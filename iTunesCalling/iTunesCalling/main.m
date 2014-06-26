22c11d82050bc0984cdf2986e2e2bd7704bd6963
edan <edanl@outlook.com>
2014-06-26 10:54:26 -0400

    Initial commit

---
+++ iTunesCalling/iTunesCalling/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTunesCalling
+//
+//  Created by Edan Lichtenstein on 6/26/14.
+//  Copyright (c) 2014 Edan Lichtenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ELAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ELAppDelegate class]));
+    }
+}
---
+++ iTunesCalling/iTunesCallingTests/iTunesCallingTests.m
@@ -0,0 +1,34 @@
+//
+//  iTunesCallingTests.m
+//  iTunesCallingTests
+//
+//  Created by Edan Lichtenstein on 6/26/14.
+//  Copyright (c) 2014 Edan Lichtenstein. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iTunesCallingTests : XCTestCase
+
+@end
+
+@implementation iTunesCallingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
